3ed958af00d69cc52205a65d8e1c781ae3de4cae
Vladimir Kozyr <kozyrdev@gmail.com>
2026-05-21 00:09:49 +0300

    add force-dynamic

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Home } from "@/pages/home";
 import { generatePageMetadata } from "@/widgets/PageSEO/PageSEO";
 
+export const dynamic = "force-dynamic";
+
 export async function generateMetadata(): Promise<Metadata> {
   return generatePageMetadata("homepage", false);
 }
